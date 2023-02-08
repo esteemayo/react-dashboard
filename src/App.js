8e4f168a75c45dbcde7d2d0abfab906fa8db94b2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-08 15:10:16 +0100

    fix: add context API to the app

--- src/App.js
+++ src/App.js
@@ -16,26 +16,29 @@ import {
 } from 'pages/index';
 
 import './App.css';
+import { darkTheme, lightTheme } from 'utils/Theme';
 
 function App() {
   const [darkMode, setdarkMode] = useState(true);
 
   return (
-    <Router>
-      <Routes>
-        <Route path='login' element={<Login />} />
-        <Route path='/' element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path='users' element={<UserList />} />
-          <Route path='user/:id' element={<User />} />
-          <Route path='new-user' element={<NewUser />} />
-          <Route path='products' element={<ProductList />} />
-          <Route path='product/:id' element={<Product />} />
-          <Route path='new-product' element={<NewProduct />} />
-        </Route>
-        <Route path='*' element={<Error />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Router>
+        <Routes>
+          <Route path='login' element={<Login />} />
+          <Route path='/' element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path='users' element={<UserList />} />
+            <Route path='user/:id' element={<User />} />
+            <Route path='new-user' element={<NewUser />} />
+            <Route path='products' element={<ProductList />} />
+            <Route path='product/:id' element={<Product />} />
+            <Route path='new-product' element={<NewProduct />} />
+          </Route>
+          <Route path='*' element={<Error />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
