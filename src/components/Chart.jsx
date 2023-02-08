e08fe7ba8574b62e293c2f30189262bab54cd7dc
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-08 20:14:53 +0100

    feat: add chart-grid class name to cartesian grid

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -18,7 +18,7 @@ const Chart = ({ title, data, dataKey, grid }) => {
           <XAxis dataKey='name' stroke='#5550bd' />
           <Line type='monotone' dataKey={dataKey} stroke='#5550bd' />
           <Tooltip />
-          {grid && <CartesianGrid stroke='#e0dfdf' strokeDasharray='5 5' />}
+          {grid && <CartesianGrid className='chart-grid' stroke='#e0dfdf' strokeDasharray='5 5' />}
           <Legend />
         </LineChart>
       </ResponsiveContainer>
