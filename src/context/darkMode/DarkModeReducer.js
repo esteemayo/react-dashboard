adf7b73d7132ceb541af8d4985b5abcbeb99ee89
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-08 15:27:50 +0100

    fix: handle all actions

--- src/context/darkMode/DarkModeReducer.js
+++ src/context/darkMode/DarkModeReducer.js
@@ -1,5 +1,28 @@
+import { DARK, LIGHT, TOGGLE } from './DarkModeTypes';
 
+const DarkModeReducer = (state, { payload, type }) => {
+  switch (type) {
+    case DARK:
+      return {
+        ...state,
+        darkMode: true,
+      };
 
-const DarkModeReducer = (state, { payload, type }) => { };
+    case LIGHT:
+      return {
+        ...state,
+        darkMode: false,
+      };
+
+    case TOGGLE:
+      return {
+        ...state,
+        darkMode: !state.darkMode,
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default DarkModeReducer;
