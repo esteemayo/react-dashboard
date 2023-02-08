da82fd3583e1677649d434557eadefcec26ea32e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-08 17:18:45 +0100

    fix: modify

--- src/components/WidgetLg.jsx
+++ src/components/WidgetLg.jsx
@@ -70,9 +70,9 @@ const WidgetLg = () => {
 const Container = styled.div`
   flex: 2;
   padding: 2rem;
-  -webkit-box-shadow: 0 0 1.5rem -1rem rgba(0, 0, 0, 0.75);
-  -moz-box-shadow: 0 0 1.5rem -1rem rgba(0, 0, 0, 0.75);
-  box-shadow: 0 0 1.5rem -1rem rgba(0, 0, 0, 0.75);
+  -webkit-box-shadow: ${({ theme }) => theme.box};
+  -moz-box-shadow: ${({ theme }) => theme.box};
+  box-shadow: ${({ theme }) => theme.box};
 `;
 
 const Title = styled.h3`
