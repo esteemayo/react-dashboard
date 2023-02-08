ae6b7ff0e15a3a7ca9d70cf0e92c87b2e51ce80e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-08 15:10:27 +0100

    fix: add context API to the app

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DarkModeProvider from 'context/darkMode/DarkModeContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DarkModeProvider>
+      <App />
+    </DarkModeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
