4303c8ef4985570629e368da3b8525c310a2f48c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-08 15:43:55 +0100

    fix: modify

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import {
   AttachMoney,
   BarChart,
@@ -12,8 +14,6 @@ import {
   TrendingUp,
   WorkOutline,
 } from '@material-ui/icons';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
