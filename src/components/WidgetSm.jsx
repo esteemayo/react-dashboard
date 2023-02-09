2f7c64223cf0048396f8b8799254f29de0990058
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-09 17:13:35 +0100

    fix: import user default avatar

--- src/components/WidgetSm.jsx
+++ src/components/WidgetSm.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { Visibility } from '@material-ui/icons';
 
 import { phone } from 'responsive';
+import defaultAvatar from 'img/avatar.png';
 
 const WidgetSm = () => {
   return (
