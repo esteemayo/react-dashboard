ce99bd3ccfa7eaca0e437a002592c1606a54e1d1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-09 08:05:37 +0100

    fix: refactor

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -16,36 +16,7 @@ const UserList = () => {
   };
 
   const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    {
-      field: 'user',
-      headerName: 'User',
-      width: 200,
-      renderCell: (params) => {
-        return (
-          <UserListUser>
-            <Image src={params.row.avatar} />
-            {params.row.username}
-          </UserListUser>
-        );
-      },
-    },
-    { field: 'avatar', headerName: 'Avatar', width: 200 },
-    {
-      field: 'email',
-      headerName: 'Email',
-      width: 200,
-    },
-    {
-      field: 'status',
-      headerName: 'Status',
-      width: 120,
-    },
-    {
-      field: 'transaction',
-      headerName: 'Transaction Volume',
-      width: 160,
-    },
+
     {
       field: 'action',
       headerName: 'Action',
@@ -87,20 +58,6 @@ const UserList = () => {
 
 const Container = styled.div`
   flex: 4;
-`;
-
-const UserListUser = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const Image = styled.img`
-  width: 3.2rem;
-  height: 3.2rem;
-  border-radius: 50%;
-  display: block;
-  object-fit: cover;
-  margin-right: 1rem;
 `;
 
 const EditButton = styled.button`
