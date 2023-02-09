ea53e8671401cff127fd41a2534afb6abb623e03
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-09 17:53:53 +0100

    fix: set default user avatar

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -28,7 +28,7 @@ const User = () => {
       <UserContainer>
         <ShowUser>
           <Top>
-            <Image src='https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto-compress&cs=tinysrgb&dpr=2&w=500' alt='' />
+            <Image src={defaultAvatar} alt='' />
             <TopTitle>
               <ShowUserUsername>Anna becker</ShowUserUsername>
               <ShowUserTitle>Software engineer</ShowUserTitle>
