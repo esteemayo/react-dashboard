ae3b28cb4a46147e11bb6cbc058d8cba968074eb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-10 14:50:53 +0100

    feat: add setToStorage function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,7 @@
 export const getFromStorage = (key) => {
   return JSON.parse(localStorage.getItem(key));
 }
+
+export const setToStorage = (key, value) => {
+  return localStorage.setItem(key, JSON.stringify(value));
+}
