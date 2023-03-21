6a92e3d0b7356628308ed47386b4354684d1fa4a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-21 09:38:37 +0100

    feat: add css properties

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -7,7 +7,12 @@ const Spinner = ({ size }) => {
 };
 
 const Container = styled.div`
-  
+  width: ${({ size }) => size === 'sm' && '2rem'};
+  width: ${({ size }) => size === 'md' && '5rem'};
+  width: ${({ size }) => size === 'lg' && '10rem'};
+  height: ${({ size }) => size === 'sm' && '2rem'};
+  height: ${({ size }) => size === 'md' && '5rem'};
+  height: ${({ size }) => size === 'lg' && '10rem'};
 `
 
 export default Spinner;
